89d8d684d6ac1d1d3c0fc7110b05c7f5000058e6
Ricky <rickdhanota@gmail.com>
2020-08-10 21:20:12 -0700

    more practice

--- sll.py
+++ sll.py
@@ -36,40 +36,59 @@
 # sll1.display()
 
 
-class Node():
-    def __init__(self, val):
-        self.val = val
-        self.next = None
+# class Node():
+#     def __init__(self, val):
+#         self.val = val
+#         self.next = None
 
-class SList():
+# class SList():
+#     def __init__(self):
+#         self.head = None
+    
+#     def add_to_front(self, val):
+#         new_node = Node(val)
+#         current_head = self.head
+#         new_node.next = current_head
+#         self.head = new_node
+#         return self
+    
+#     def print_values(self):
+#         runner = self.head
+#         while (runner != None):
+#             print(runner.val)
+#             runner = runner.next
+#         return self
+    
+#     def add_to_back(self, val):
+#         if self.head == None:
+#             self.add_to_front(val)
+#             return self
+
+#         new_node = Node(val)
+#         runner = self.head
+#         while (runner.next != None):
+#             runner = runner.next
+#         runner.next = new_node
+#         return self
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = next
+
+class SLL:
     def __init__(self):
         self.head = None
     
-    def add_to_front(self, val):
-        new_node = Node(val)
-        current_head = self.head
-        new_node.next = current_head
-        self.head = new_node
-        return self
-    
-    def print_values(self):
+    def add_Node(self, value):
+        new_node = Node(value)
         runner = self.head
-        while (runner != None):
-            print(runner.val)
-            runner = runner.next
-        return self
-    
-    def add_to_back(self, val):
-        if self.head == None:
-            self.add_to_front(val)
-            return self
-            
-        new_node = Node(val)
-        runner = self.head
-        while (runner.next != None):
+
+        while runner.next != None:
             runner = runner.next
         runner.next = new_node
-        return self
+        
 
 
 
